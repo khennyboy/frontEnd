6cf5ae67b4df33cb674c4a3fe60449d640504b48
Khennyboy <abdullateefkehinde848@gmail.com>
2024-01-23 08:47:50 +0100

    second project

--- static-job-listings-master/index.js
+++ static-job-listings-master/index.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', function(){
 
 let parent = document.querySelector('.wrapper')
+let searchContainer = document.querySelector('.search')
+let searchContainerValue;
+
 
 for(var i=0;  i < datas.length; i++){
-
     let m = datas[i], x;
  x = `<div class="each_role" id='each_role${m.id}'>
 <div class="left">
@@ -24,8 +26,8 @@ for(var i=0;  i < datas.length; i++){
 </div>
 </div>
 <div class="right">
-<span class="language" data-language="${m.role}">${m.role}</span>
-<span class="language" data-language="${m.level}">${m.level}</span>
+<span class="language first" data-language="${m.role.toLowerCase()}">${m.role}</span>
+<span class="language" data-language="${m.level.toLowerCase()}">${m.level}</span>
 </div>
 </div>`
 
@@ -35,12 +37,12 @@ let right = document.querySelector(`#each_role${m.id} .right`)
 let info_1 = document.querySelector(`#each_role${m.id} .left .info_1`)
 
 m.languages.forEach(each=>{
-  let tag = `<span class="language" data-language="${each}">${each}</span>`
+  let tag = `<span class="language" data-language="${each.toLowerCase()}">${each}</span>`
   right.insertAdjacentHTML('beforeend', tag)
 })
 
 m.tools.forEach(each=>{
-    let tag = `<span class="language" data-language="${each}">${each}</span>`
+    let tag = `<span class="language" data-language="${each.toLowerCase()}">${each}</span>`
     right.insertAdjacentHTML('beforeend', tag)
   })
 
@@ -48,5 +50,67 @@ m.tools.forEach(each=>{
   m.featured && info_1.insertAdjacentHTML('beforeend', `<span class="featured">FEATURED</span>` )
 
 }
+
+let all_roles = document.querySelectorAll('.language')
+let child = document.querySelectorAll('.each_role')
+let rights = document.querySelectorAll('.right')
+// let roles = []
+
+
+// all_roles.forEach(each=> roles.push((each.dataset['language'])))
+// roles = [... new Set(roles)]
+
+searchContainer.addEventListener('keyup', function(e){
+  e.target.value = e.target.value.toLowerCase()
+  
+  if(e.target.value!=''){
+  child.forEach(each=>each.style.display='none')
+  searchContainerValue = searchContainer.value.split(' ').filter(each=>{
+     if(each!=''){
+      return each;
+    }
+  });
+  console.log(searchContainerValue)
+
+  //function to check if all input exist in a div with class right as children
+  function handleChildrenWithClasses(element, classList){
+    for(const className of classList){
+        if(!element.querySelector(`.language[data-language="${className}"]`)){
+            return false
+        }
+    }
+    return true
+}
+
+for (const right of rights){
+    if(handleChildrenWithClasses(right, searchContainerValue)){
+        right.closest('.each_role').style.display= 'block'
+    }
+    else{
+      console.error('no result for this search')
+    }
+}
+  
+  // searchContainerValue.forEach(each=>{
+  //   if(roles.includes(each)){
+  //   //  console.log('hello')
+  //    let matchingElements = document.querySelectorAll(`.language[data-language="${each}"]`);
+  //   //  console.log(matchingElements)
+  //    matchingElements.forEach(each_tag=>{
+  //    each_tag.closest('.each_role').style.display='block'
+  //    })
+  //  }
+  //  else{
+  //    console.error('welcome')
+  //  } 
+  //  })
+  }
+
+  else{
+    child.forEach(each=>each.style.display='block')
+  }
+
+})
+
 });
 
