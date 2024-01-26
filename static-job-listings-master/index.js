a0cc2ea758405073281d72918ea2f3d7bce652c3
khennyboy <abdullateefkehinde848@gmail.com>
2024-01-26 23:32:20 +0100

    refactored

--- static-job-listings-master/index.js
+++ static-job-listings-master/index.js
@@ -85,31 +85,37 @@ function handleChildrenWithClasses(all_search, searchContainerValue){
     for(const each_search of all_search){
       let pos = each_search.indexOf(each_input)
      if(pos!=-1){
-      // if(each_input==each_search){
-      //    store.push(each_search)
-      //    console.log(store)
-      // }
+      if(each_input==each_search){
+         store.push(each_search)
+         console.log(store)
+      }
      
      let rights = document.querySelectorAll('.right')
      for(each of rights){
-      // if(store.length!=0){
-
-      // }
         if(each.querySelector(`.language[data-language=${each_search}]`)){
-          each.closest('.each_role').style.display='block'
+          // each.closest('.each_role').style.display='block'
+          //check if the latest result also  has the previous search input
+          if(store.length!=0 ){
+            var x = store.every(previous=>{
+            each.querySelector(`.language[data-language=${previous}`)
+            })
+            console.log(x)
+          }
+          else if(store.length==0){
+            each.closest('.each_role').style.display='block'
+          }
+          if(x){
+            each.closest('.each_role').style.display='block'
+          }
+          
         }
      }
      }
   }
 }
+// end of the handleChildrenWithClasses function here
 }
 
 });
 
-
-  //  x.forEach(each=>{
-    //   each.closest('.each_role').style.display='block'
-    //  })
-
-        //  var x =  document.querySelectorAll(`.language[data-language=${each_search}]`)
 
