b2c87e211f37114edb682041bad8db64675d191e
Khennyboy <abdullateefkehinde848@gmail.com>
2024-02-14 00:01:50 +0100

    done

--- url-shortening-api-master/index.js
+++ url-shortening-api-master/index.js
@@ -1,7 +1,23 @@
-
-const wait = function (seconds) {
-    return new Promise(function (resolve) {
-      setTimeout(resolve, seconds * 1000);
-    });
-  };
+var url = 'https://khennyboy.github.io/Hackathon'
+const formData = new FormData()
+formData.append('link', encodeURIComponent(url))
+const shortUrl = async function(){
+  const settings = {
+    header: {
+        'Accept': 'application/json'
+    },
+    method: 'POST',
+    body: formData
+  }
+  try{
+  const get = await fetch('https://riganapi.pythonanywhere.com/api/v1/url/shorten/', settings)
+  const data = await get.json()
+  console.log(data)
+  console.log(data.data)
+  }
+  catch(error){
+    console.error(error.message)
+  }
+}
+shortUrl()
 
