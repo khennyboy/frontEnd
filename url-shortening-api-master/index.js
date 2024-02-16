772dd788c33024180336b4d19ced7d06630184fb
Khennyboy <abdullateefkehinde848@gmail.com>
2024-02-17 00:34:14 +0100

    done

--- url-shortening-api-master/index.js
+++ url-shortening-api-master/index.js
@@ -1,16 +1,15 @@
-
-
-var url = 'https://khennyboy.github.io/Hackathon'
+window.addEventListener('load', function(){
+  var url = 'https://khennyboy.github.io/Hackathon'
 const formData = new FormData()
-formData.append('link', encodeURIComponent(url))
-const shortUrl = async function(){
-  const settings = {
-    header: {
-        'Accept': 'application/json'
-    },
-    method: 'POST',
-    body: formData
-  }
+  formData.append('link', encodeURIComponent(url))
+  const shortUrl = async function(){
+    const settings = {
+      header: {
+          'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: formData
+    }
   try{
   const get = await fetch('https://riganapi.pythonanywhere.com/api/v1/url/shorten/', settings)
   const data = await get.json()
@@ -22,4 +21,42 @@ const shortUrl = async function(){
   }
 }
 shortUrl()
+
+
+let nav_icon = document.querySelector('.icons')
+let nav_content = document.querySelector('.nav_links')
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && nav_icon.classList.contains('change')) {
+    nav_icon.classList.remove('change')
+    nav_content.classList.remove('show')
+  }
+});
+
+  nav_icon.addEventListener('click', function(e){
+  nav_icon.classList.toggle('change')
+  e.stopPropagation()
+  if(nav_icon.classList.contains('change')){
+    nav_content.classList.add('show')
+  }
+else{
+  nav_content.classList.remove('show')
+}
+})
+document.addEventListener('click', function(){
+  if(nav_icon.classList.contains('change') ){
+    nav_icon.classList.remove('change')
+    nav_content.classList.remove('show')
+    }
+})
+window.addEventListener('resize', function(){
+if(nav_icon.classList.contains('change')){
+  nav_content.classList.remove('show')
+  nav_icon.classList.remove('change')
+}
+})
+
+})
+
+
 
