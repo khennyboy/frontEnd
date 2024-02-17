10310e7c5ce28107d941a5fbe029b29691c1dad7
Khennyboy <abdullateefkehinde848@gmail.com>
2024-02-17 08:03:40 +0100

    done

--- url-shortening-api-master/index.js
+++ url-shortening-api-master/index.js
@@ -56,7 +56,25 @@ if(nav_icon.classList.contains('change')){
 }
 })
 // animation functions
+const allSections = document.querySelectorAll('.section')
 
+
+const revealSection = function(entries, sectionObserver){
+  const [entry] = entries
+  if(!entry.isIntersecting){
+    return 
+  }
+  entry.target.classList.remove('section--hidden')
+  sectionObserver.unobserve(entry.target)
+}
+const sectionObserver = new IntersectionObserver(revealSection, {
+root:null,
+threshold: 0.25
+})
+allSections.forEach(function(section){
+  sectionObserver.observe(section)
+  section.classList.add('section--hidden')
+})
 })
 
 
